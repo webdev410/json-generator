36279ab889b1417f20fef200213a5fc42419f51b
Andrew Keiser <webdev410@gmail.com>
2021-09-18 17:15:55 -0700

    enter key = addBtn

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const keyEl = document.getElementById("key");
 const valueEl = document.getElementById("value");
 const addBtn = document.getElementById("add-another");
 const jsonResult = document.querySelector("#jsonResult");
-const deleteBtn = document.querySelectorAll('.deleteBtn')
-const generateBtn = document.getElementById('generateBtn')
+const deleteBtn = document.querySelectorAll(".deleteBtn");
+const generateBtn = document.getElementById("generateBtn");
 
 let finalJson;
 
@@ -22,18 +22,23 @@ function addKeyValue(event) {
     const newFormGroup = document.createElement("div");
     const newKey = document.createElement("input");
     const newValue = document.createElement("input");
-    const deleteBtn = document.createElement('button')
+    const deleteBtn = document.createElement("button");
 
-
-    newFormGroup.classList.add('kv-pair')
-    deleteBtn.classList.add('deleteBtn')
-    deleteBtn.innerHTML = '<i class="fas fa-trash-alt deleteBtn"></i>'
+    newFormGroup.classList.add("kv-pair");
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.innerHTML = '<i class="fas fa-trash-alt deleteBtn"></i>';
 
     newKey.classList.add("key");
     newValue.classList.add("value");
 
-    newFormGroup.setAttribute('data-item-id', additionalFields.childElementCount + 1)
-    deleteBtn.setAttribute('data-item-id', additionalFields.childElementCount + 1)
+    newFormGroup.setAttribute(
+        "data-item-id",
+        additionalFields.childElementCount + 1
+    );
+    deleteBtn.setAttribute(
+        "data-item-id",
+        additionalFields.childElementCount + 1
+    );
     newKey.setAttribute("placeholder", "key");
     newKey.setAttribute("type", "text");
 
@@ -48,42 +53,39 @@ function addKeyValue(event) {
     additionalFields.appendChild(newFormGroup);
 
     if (additionalFields.childElementCount > 0) {
-        generateBtn.classList.remove('hide')
+        generateBtn.classList.remove("hide");
     }
-
 }
 
 function generateJson(event) {
     event.preventDefault();
 
     // if key is empty, throw error
-    let key = document.querySelectorAll('.key')
+    let key = document.querySelectorAll(".key");
     // console.log("Key Length: ", key.length)
 
     for (var i = 0; i < key.length; i++) {
         // console.log("Value: ", key[i].value)
         if (!key[i].value) {
-            console.log('key-value is empty')
-            alert('please remove unused inputs')
-            return
-
+            console.log("key-value is empty");
+            alert("please remove unused inputs");
+            return;
         }
     }
     // create copy button
-    let row = document.createElement('div')
-    row.classList.add('row')
-    row.classList.add('justify-content-end')
+    let row = document.createElement("div");
+    row.classList.add("row");
+    row.classList.add("justify-content-end");
 
-    let copyBtn = document.createElement('button')
-    copyBtn.textContent = "Copy"
-    copyBtn.classList.add('copyBtn')
-    copyBtn.classList.add('btn-light')
-    copyBtn.classList.add('btn-sm')
-    copyBtn.innerHTML = '<i class="fal fa-copy"></i>'
-    copyBtn.setAttribute('onClick', 'copyText()')
+    let copyBtn = document.createElement("button");
+    copyBtn.textContent = "Copy";
+    copyBtn.classList.add("copyBtn");
+    copyBtn.classList.add("btn-light");
+    copyBtn.classList.add("btn-sm");
+    copyBtn.innerHTML = '<i class="fal fa-copy"></i>';
+    copyBtn.setAttribute("onClick", "copyText()");
 
-    row.appendChild(copyBtn)
-
+    row.appendChild(copyBtn);
 
     const outputObj = {};
     const inputs = Array.prototype.slice.call(
@@ -105,34 +107,32 @@ function generateJson(event) {
 
     // add to div
     jsonResult.textContent = finalJson;
-    jsonResult.appendChild(row)
+    jsonResult.appendChild(row);
     // show json object
     jsonResult.classList.remove("hide");
     copyBtn.classList.remove("hide");
 }
 
 function deleteItem(itemId, formId) {
-    event.preventDefault()
+    event.preventDefault();
 
-    let deleteDiv = document.querySelector('.kv-pair')
+    let deleteDiv = document.querySelector(".kv-pair");
     if (itemId === formId) {
-        deleteDiv.remove()
+        deleteDiv.remove();
     }
 }
 
 function copyText() {
-    let range = document.createRange()
+    let range = document.createRange();
     range.selectNode(jsonResult);
     window.getSelection().removeAllRanges(); // clear current selection
     window.getSelection().addRange(range); // to select text
 
     document.execCommand("copy");
-    window.getSelection().removeAllRanges();// to deselect
+    window.getSelection().removeAllRanges(); // to deselect
 
-
-    alert("Copied!")
+    alert("Copied!");
 }
-
 
 additionalFields.addEventListener("click", (event) => {
     event.preventDefault();
@@ -140,9 +140,26 @@ additionalFields.addEventListener("click", (event) => {
         let itemId = event.target.getAttribute("data-item-id");
         let formId = event.target.getAttribute("data-item-id");
         deleteItem(itemId, formId);
-
     }
 });
+// Enter Key = click addBtn
+window.addEventListener(
+    "keydown",
+    function (e) {
+        if (
+            e.keyIdentifier == "U+000A" ||
+            e.keyIdentifier == "Enter" ||
+            e.keyCode == 13
+        ) {
+            if (e.target.nodeName == "INPUT" && e.target.type == "text") {
+                e.preventDefault();
+                addBtn.click()
+                return false;
+            }
+        }
+    },
+    true
+);
 
 form.addEventListener("submit", generateJson);
 addBtn.addEventListener("click", addKeyValue);
